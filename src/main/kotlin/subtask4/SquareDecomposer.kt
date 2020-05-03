dbe18a32c73db026adcca2fca2b2ad8715b2d9d7
Andrei K <senevit$@yandex.ru>
2020-05-03 18:28:48 +0300

    feat: task 4 implementation added

--- src/main/kotlin/subtask4/SquareDecomposer.kt
+++ src/main/kotlin/subtask4/SquareDecomposer.kt
@@ -1,9 +1,42 @@
 package subtask4
 
+import java.util.*
+
+
 class SquareDecomposer {
 
-    // TODO: Complete the following function
+
     fun decomposeNumber(number: Int): Array<Int>? {
-        throw NotImplementedError("Not implemented")
+        val numberLong = number.toLong()
+        val decomposedList = decomposer(numberLong, numberLong * numberLong)
+
+        return if (decomposedList != null) {
+            decomposedList.removeAt(decomposedList.size -1)
+            decomposedList.toTypedArray()
+        } else {
+            null
+        }
+
+    }
+
+    private fun decomposer(n: Long, remain: Long): MutableList<Int>? {
+        if (remain == 0.toLong()) {
+            val resultList: MutableList<Int> = ArrayList()
+            resultList.add(n.toInt())
+            return resultList
+        }
+
+        for (i in n - 1 downTo 1) {
+            val difference = remain - i * i
+            if (difference >= 0) {
+                val resultList = decomposer(i, difference)
+                if (resultList != null) {
+                    resultList.add(n.toInt())
+                    return resultList
+                }
+            }
+        }
+
+        return null
     }
 }
